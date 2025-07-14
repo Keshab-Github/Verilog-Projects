5199664a0e96dd49d416359e7de88a8d5a4646ad
Keshab-Github <155210206+Keshab-Github@users.noreply.github.com>
2025-07-15 03:33:59 +0530

    Add files via upload

---
+++ User-Defined Primitives/SR flip-flop/udp_SR_FF.v
@@ -0,0 +1,15 @@
+// A positive edge sensitive SR flip-flop
+primitive SRFF(q, s, r, clk, clr);
+    input s,r,clk, clr;
+    output reg q;
+    table
+    // s r clk clr  q  q_new
+       ? ?  ?   1 : ? : 0;   // clear
+       ? ?  ? (10): ? : -;   // ignore...(no change)
+       0 0 (01) 0 : ? : -;   // no change
+       0 1 (01) 0 : ? : 0;   // reset condition 
+       1 0 (01) 0 : ? : 1;   // set condition 
+       1 1 (01) 0 : ? : x;   // invalid condition
+       ? ? (10) 0 : ? : -;   // ignore...(no change)
+    endtable
+endprimitive

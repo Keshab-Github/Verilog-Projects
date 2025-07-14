2a2406f1d6b6f56311fada373376c9d873ce0335
Keshab-Github <155210206+Keshab-Github@users.noreply.github.com>
2025-07-15 03:10:57 +0530

    Add files via upload

---
+++ CMOS NAND gate/CMOS_NAND_test.v
@@ -0,0 +1,14 @@
+module cmosnand_test;
+    reg in1, in2;
+    wire out;
+    integer k;
+    cmosnand MYNAND2(in1, in2, out);
+    initial
+        begin
+            for(k=0;k<4;k++)
+                begin
+                #5 {in1, in2}=k;
+                $display("In1:%b, In2:%b, Out:%b", in1, in2, out);
+                end
+        end
+endmodule
